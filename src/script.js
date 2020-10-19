c5f7fd147d37e6411e4f2c031704750a411690ab
Raymond Baguma <you@example.com>
2020-10-19 15:50:26 +0200

    initial commit

---
+++ src/script.js
@@ -0,0 +1,27 @@
+const HOURHAND = document.querySelector("#hour");
+const MINUTEHAND = document.querySelector("#minute");
+const SECONDHAND = document.querySelector("#second");
+
+const date = new Date();
+let hr = date.getHours();
+let min = date.getMinutes();
+let sec = date.getSeconds();
+
+console.log('Hour: ' + hr + ' Minute: ' + min + ' Second: ' + sec);
+
+let hrPosition = (hr * 360) / 12 + (min * (360 / 60) / 12);
+let minPosition = (min * 360) + (sec * (360 / 60) / 60);
+let secPosition = (sec * 360) / 60;
+
+function runTime() {
+    hrPosition = hrPosition + (3 / 360);
+    minPosition = minPosition + (6 / 60);
+    secPosition = secPosition + 6;
+
+    HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
+    MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+    SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+
+}
+
+let interval = setInterval(runTime, 1000);
